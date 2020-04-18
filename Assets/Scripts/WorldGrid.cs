131f93c7e2731c8ccce245300dd4243fb1677aff
unknown <maigmarion@gmail.com>
2020-04-18 13:05:04 +0200

    start on grid world (temp grid shader)

---
+++ Assets/Scripts/WorldGrid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WorldGrid : MonoBehaviour {
+
+	public int gridWidth;
+	public int gridHeight;
+
+	private bool[] _map;
+
+	void Awake() {
+		_map = new bool[gridWidth * gridHeight];
+	}
+
+	private int FromCoord(int pX, int pY) {
+		return pX + gridWidth * pY;
+	}
+
+	private (int, int) FromIndex(int pIndex) {
+		return (pIndex % gridWidth, pIndex / gridWidth);
+	}
+
+}
