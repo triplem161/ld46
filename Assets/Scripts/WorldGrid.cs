44d132d2ab376c23fc40fbf7cbba5c898ee945c7
unknown <maigmarion@gmail.com>
2020-04-18 17:04:25 +0200

    fixed coordinate to position

--- Assets/Scripts/WorldGrid.cs
+++ Assets/Scripts/WorldGrid.cs
@@ -15,18 +15,35 @@ public class WorldGrid : MonoBehaviour {
 
 	void Awake() {
 		_map = new int[] {
-			1, 0, 0, 0, 1,
+			1, 0, 0, 0, 0,
 			0, 0, 0, 0, 0,
-			0, 0, 1, 0, 0,
 			0, 0, 0, 0, 0,
-			1, 0, 0, 0, 1
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1
 		};
 
 		GameObject vTemp;
 		for (int i = 0; i < _map.Length; ++i) {
 			if (_map[i] > 0) {
 				vTemp = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				vTemp.transform.localPosition = ToLocalPosition(i);
+				vTemp.transform.localPosition = IndexToPosition(i) + new Vector3(0.5f, 0, -0.5f);
+			}
+		}
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			var vRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit vHit = new RaycastHit();
+			int vLayer = LayerMask.GetMask("GRID_COLLISION");
+			if (Physics.Raycast(vRay, out vHit, Mathf.Infinity, vLayer)) {
+
+				// Debug.Log("Clicked at: " + vHit.point);
+				// Debug.Log("Clicked at coord: " + PositionToCoord(vHit.point));
+				// GameObject vTemp;
+				// vTemp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				// (int, int) vCoord = PositionToCoord(vHit.point);
+				// vTemp.transform.localPosition = CoordToPosition(vCoord.Item1, vCoord.Item2);
 			}
 		}
 	}
@@ -36,16 +53,27 @@ public class WorldGrid : MonoBehaviour {
 		debugRenderer.material.SetVector("GRID_SIZE", new Vector4(gridWidth, gridHeight, 0, 0));
 	}
 
-	private int FromCoord(int pX, int pY) {
+	private int CoordToIndex(int pX, int pY) {
 		return pX + gridWidth * pY;
 	}
 
-	private (int, int) FromIndex(int pIndex) {
+	private (int, int) IndexToCoord(int pIndex) {
 		return (pIndex % gridWidth, pIndex / gridWidth);
 	}
 
-	private Vector3 ToLocalPosition(int pIndex) {
-		Vector3 vOffset = new Vector3(MESH_UNIT / gridWidth, 0, MESH_UNIT / gridHeight);
-		return new Vector3(FromIndex(pIndex).Item1, 0.5f, FromIndex(pIndex).Item2) - vOffset;
+	private Vector3 IndexToPosition(int pIndex) {
+		(int, int) vCoord = IndexToCoord(pIndex);
+		Vector3 vOffset = new Vector3(gridWidth / 2f, 0, gridHeight / 2f);
+		Vector3 vOrigin = new Vector3(-vOffset.x, 0.5f, vOffset.z);
+		return new Vector3(vOrigin.x + vCoord.Item1, 0.5f, vOrigin.z - vCoord.Item2);
+	}
+
+	private Vector3 CoordToPosition(int pX, int pY) {
+		return IndexToPosition(CoordToIndex(pX, pY));
+	}
+
+	private (int, int) PositionToCoord(Vector3 pPosition) {
+		Vector3 vOffset = new Vector3(gridWidth / 2f, 0, gridHeight / 2f);
+		return (Mathf.FloorToInt(pPosition.x + vOffset.x), Mathf.FloorToInt(vOffset.z - pPosition.z));
 	}
 }
