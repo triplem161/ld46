d03360c2dea7f8634c1cadc73595b4edbac5d755
unknown <maigmarion@gmail.com>
2020-04-19 10:56:41 +0200

    cube movement

--- Assets/Scripts/SCE_main.cs
+++ Assets/Scripts/SCE_main.cs
@@ -17,7 +17,7 @@ public class SCE_main : MonoBehaviour {
 			var vRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit vHit = new RaycastHit();
 			if (Physics.Raycast(vRay, out vHit, Mathf.Infinity, _gridLayer)) {
-				grid.PullMagnets(vHit.point);
+				grid.PullOrder(vHit.point);
 			}
 		}
 	}
