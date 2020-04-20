94e2fd8b054e3ae72d3378585d20c0ebff1b6de6
Taldius <antoinesemblat@hotmail.fr>
2020-04-20 10:31:03 +0200

    Adding score script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private void OnEnable() {
+        
+    }
+
+    private void OnDisable() {
+        
+    }
+
+    private void UpdateScore(ScoreEvent pEvent) {
+        
+    }
+}
